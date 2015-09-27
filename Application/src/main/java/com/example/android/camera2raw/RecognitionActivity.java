45b4f61affbcfba41850dad5f04af2e44e3d0c4c
Sergii Khomenko <sergii.khomenko@stylight.com>
2015-09-27 05:38:12 +0200

    fixed words api

--- Application/src/main/java/com/example/android/camera2raw/RecognitionActivity.java
+++ Application/src/main/java/com/example/android/camera2raw/RecognitionActivity.java
@@ -18,10 +18,14 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
-
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 
 import com.clarifai.api.ClarifaiClient;
@@ -29,6 +33,8 @@ import com.clarifai.api.RecognitionRequest;
 import com.clarifai.api.RecognitionResult;
 import com.clarifai.api.Tag;
 import com.clarifai.api.exception.ClarifaiException;
+
+import javax.net.ssl.HttpsURLConnection;
 
 import static android.provider.MediaStore.Images.Media;
 
@@ -194,5 +200,79 @@ public class RecognitionActivity extends Activity {
             textView.setText("Sorry, there was an error recognizing your image.");
         }
         selectButton.setEnabled(true);
+    }
+
+    public class WordsAPI extends AsyncTask<String, Void, String>{
+
+        private String baseUrl = "https://wordsapiv1.p.mashape.com/words/";
+        private String key;
+
+        public WordsAPI(String key) {
+            this.key = key;
+        }
+
+        private HttpsURLConnection createConnection(String word, String method) {
+            String query = baseUrl + word + "/" + method;
+            URL url = null;
+            try {
+                url = new URL(query);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+            HttpsURLConnection conn = null;
+            try {
+                conn = (HttpsURLConnection) url.openConnection();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            conn.setRequestProperty("X-Mashape-Key", key);
+            conn.setRequestProperty("Accept", "application/json");
+            return conn;
+        }
+
+        public String getDefinition(String word) throws IOException {
+            HttpsURLConnection conn = createConnection(word, "definition");
+
+            InputStreamReader in = new InputStreamReader((InputStream) conn.getContent());
+            BufferedReader buff = new BufferedReader(in);
+            StringBuilder builder = new StringBuilder();
+            String line;
+            do {
+                line = buff.readLine();
+                if (line != null) {
+                    builder.append(line + "\n");
+                }
+            } while (line != null);
+
+            return builder.toString();
+        }
+
+
+        @Override
+        protected String doInBackground(String... words) {
+
+            for (String word : words) {
+                try {
+                    return getDefinition(word);
+                } catch (IOException e) {
+                    Log.d("WordsAPI", "Word not found" + word);
+//                e.printStackTrace();
+                }
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            handleWords(result);
+        }
+    }
+
+    private String handleWords(String definition) {
+        //handle value
+        Log.d("more words", "words have arrived: " + definition);
+
+        return definition;
     }
 }
